54eb75f4e68037501697fde6eec70b0f15347d90
Keeya Emmanuel <keeyaemmanuel12@gmail.com>
2021-09-07 08:34:55 +0300

    adding web UI skin (we are using Sing template html version) the free version

---
+++ access/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index, name='index'),
+]
+
+
+# Super Trader , Trading beyond the matrix (Van Tharp)
+# To trade or not to trade, come into my trading room (Alexandar elder)
+# Market wizard books (Jack schwager)
--- kintu/urls.py
+++ kintu/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("site/", include("landing.urls")),
+    path('', include("access.urls"))
 ]
